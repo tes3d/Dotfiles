532fba55d67b23de16ac36c695b666cfdb8f2c97
tes <tes@tes.com>
2017-12-22 11:53:10 +0900

    commit

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ if [ -d $LOCAL_REPOSITORY_PATH ]; then
       files=($(getManagedDotfiles))
       for file in ${files[@]}; do
         if [ -e "$HOME/$file" ]; then
-          rm "$HOME/$file"
+          rm -d "$HOME/$file"
         fi
       done
 
